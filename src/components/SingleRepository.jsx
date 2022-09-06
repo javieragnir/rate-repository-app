9c518df9673ad63bb779b316e796b71d2d6a4087
javier <jjr.agnir@gmail.com>
2022-09-06 12:20:54 +0800

    finish 10.20

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,25 +1,72 @@
-import { View, FlatList } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 import { useParams } from 'react-router-native';
 import useRepository from '../hooks/useRepository';
 import RepositoryItem from './RepositoryItem';
 import Text from './Text';
 import theme from '../theme';
 
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  reviewContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 15,
+    backgroundColor: 'white',
+  },
+  circleContainer: {
+    height: 40,
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    borderColor: theme.colors.primary,
+    borderWidth: 2,
+    marginRight: 15,
+  },
+  reviewInfoContainer: {
+    display: 'flex',
+    flex: 1,
+  },
+  blueText: {
+    color: theme.colors.primary,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
 const RepositoryInfo = ({ repository }) => {
-
-  return <RepositoryItem {...repository} show={true} />
-}
-
-const ReviewItem = ({ review }) => {
-  console.log(review)
-  console.log('$$$$', review.user)
 
   return (
     <View>
-      <Text>{review.rating}</Text>
-      <Text>{review.text}</Text>
-      <Text>{review.createdAt}</Text>
-      <Text>{review.user.username}</Text>
+      <RepositoryItem {...repository} show={true} />
+      <View style={styles.separator} />
+    </View>
+  )
+}
+
+const ReviewItem = ({ review }) => {
+  const date = new Date(review.createdAt);
+  let dd = date.getDay();
+  let mm = date.getMonth() + 1;
+  const yyyy = date.getFullYear();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+
+  const createdAt = `${dd}.${mm}.${yyyy}`
+
+  return (
+    <View style={styles.reviewContainer}>
+      <View style={styles.circleContainer}>
+        <Text fontWeight="bold" fontSize="subheading" style={styles.blueText}>{review.rating}</Text>
+      </View>
+      <View style={styles.reviewInfoContainer}>
+        <Text fontWeight="bold" fontSize="subheading" style={{ marginBottom: 3}}>{review.user.username}</Text>
+        <Text color="textSecondary" style={{ marginBottom: 5}}>{createdAt}</Text>
+        <Text>{review.text}</Text>
+      </View>
     </View>
   )
 };
@@ -32,13 +79,12 @@ const SingleRepository = () => {
     ? repository.reviews.edges.map((edge) => edge.node)
     : [];
 
-  console.log(reviews);
-
   return (
     <FlatList
       data={reviews}
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
+      ItemSeparatorComponent={ItemSeparator}
       ListHeaderComponent={() => <RepositoryInfo repository={repository} />}
       // ...
     />
